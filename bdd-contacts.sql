d571a964a47916fb5132aee865ec455b46b77e7a
SamuelGuidarelli <samuelguidarelli2@gmail.com>
2023-11-23 11:29:49 +0100

    création table contacts

--- bdd-contacts.sql
+++ bdd-contacts.sql
@@ -3,3 +3,15 @@ CREATE DATABASE bdd_contact_sql CHARACTER set = 'utf8' COLLATE = 'utf8_general_c
 
 USE bdd_contact_sql; 
 
+CREATE TABLE contacts (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(70),
+    prenom VARCHAR(70),
+    date_de_naissance DATE,
+    sexe VARCHAR(10),
+    adresse TEXT,
+    cp VARCHAR(10),
+    ville VARCHAR(70),
+    pays_iso_3 CHAR(3),
+    FOREIGN KEY (pays_iso_3) REFERENCES pays(iso_3)
+); 
