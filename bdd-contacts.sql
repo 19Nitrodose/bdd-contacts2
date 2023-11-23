0d699d7467b70ba018c38d8f50f18bdb99e42cc0
SamuelGuidarelli <samuelguidarelli2@gmail.com>
2023-11-23 11:36:33 +0100

    creation table + insert donne table pays

--- bdd-contacts.sql
+++ bdd-contacts.sql
@@ -3,6 +3,22 @@ CREATE DATABASE bdd_contact_sql CHARACTER set = 'utf8' COLLATE = 'utf8_general_c
 
 USE bdd_contact_sql; 
 
+CREATE TABLE pays (
+    iso_3 CHAR(3) PRIMARY KEY, 
+    nom VARCHAR(70),
+    iso_2 CHAR(2), 
+    nationalite VARCHAR(50)
+);
+
+INSERT INTO pays (iso_3, nom, iso_2, nationalite) VALUES
+    ('FRA', 'France', 'FR', 'Français'),
+    ('CHE', 'Suisse', 'CH', 'Suisse'),
+    ('TUR', 'Turquie', 'TR', 'Turc'),
+    ('BEL', 'Belgique', 'BE', 'Belge');
+   ('DEU', 'Allemagne', 'DE', 'Allemand'),
+    ('ESP', 'Espagne', 'ES', 'Espagnol'),
+    ('ITA', 'Italie', 'IT', 'Italien');
+
 CREATE TABLE contacts (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nom VARCHAR(70),
